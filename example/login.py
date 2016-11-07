610d352aed8cb963a01617f069d723b6dd976ebf
Jinhwan Choi <jinhwanlazy@gmail.com>
2016-11-08 04:18:02 +0900

    add example: login.py

---
+++ example/login.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("..")
+
+from xing.xasession import Session
+from xing.query import Query
+from getpass import getpass
+
+userid = input("Id: ")
+passwd = getpass()
+session = Session()
+session.login(id=userid, passwd=passwd)
+
+print(session.account())
+print(Query("서버시간조회").send())
+print(Query("t0167").send())
+print(session.heartbeat())
